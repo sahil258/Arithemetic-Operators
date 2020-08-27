c7d8729dc3d8113aca420d1e126b6244df5d7169
sahil258 <70344390+sahil258@users.noreply.github.com>
2020-08-28 00:47:23 +0530

    Add files via upload

---
+++ arithematic operators.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=25,b=10,c;
+
+    c=a+b;
+    printf("The addition of a and b is.: %d \n",c);
+
+    c=a-b;
+    printf("the subtraction of a and b.: %d \n",c);
+
+    c=a*b;
+    printf("the multiplication of a and b.: %d \n",c);
+
+    c=a/b;
+    printf("the division of a and b.: %d \n",c);
+
+    c=a%b;
+    printf("the remainder is.: %d \n",c);
+
+    return 0;
+}
